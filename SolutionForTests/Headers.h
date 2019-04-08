0f88f83a9a0b7f07effdf8ae5515e77d8677d8bc
Pavel Shipulin <pavel.shipulin66@outlook.com>
2019-04-09 00:11:09 +0300

    includes

--- SolutionForTests/Headers.h
+++ SolutionForTests/Headers.h
@@ -1,2 +1,8 @@
 #pragma once
 #include <iostream>
+#include <cpprest/http_client.h>
+#include <cpprest/uri.h>
+#include <cpprest/uri_builder.h>
+#include <cpprest/json.h>
+#include <cpprest/streams.h>
+#include <cpprest/filestream.h>
