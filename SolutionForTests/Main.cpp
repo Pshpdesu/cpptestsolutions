a21193e4211a47b11418197367a9b7bcc712f8f0
Pavel Shipulin <pavel.shipulin66@outlook.com>
2019-04-23 22:46:27 +0300

    N-th try

--- SolutionForTests/Main.cpp
+++ SolutionForTests/Main.cpp
@@ -1,5 +1,57 @@
-#include "Headers.h"
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+#include <numeric>
+#include <algorithm>
+using namespace std;
+
+//You are gay (╬▔皿▔)╯
+
+const int nGroups = 4;
+
+int FindMax(std::vector<int>&& gays) {
+	int gayscount = gays.size();
+	size_t maxgays = gays.size();
+	
+	for (int i = 0; i < gayscount - 1; ++i) {
+		int j = gayscount-2;
+		while (gays[i] > 0 && j>=i) {
+			if (i + j + 1 < gayscount) {
+				int neededcount = min(gays[i], gays[j]);
+				if (i == j) {
+					neededcount /= 2;
+					gays[i] -= neededcount*2;
+					gays[i + j + 1] += neededcount;
+				}
+				else {
+					gays[i] -= neededcount;
+					gays[j] -= neededcount;
+					gays[i + j + 1] += neededcount;
+				}
+			}
+			j--;
+			/*int possiblefillofgays = gays[i] / (j+1);
+			if (possiblefillofgays != 0) {
+				gays[j] -= possiblefillofgays;
+				gays[i] -= possiblefillofgays * (maxgays - j+1);
+				j--;
+			} else {
+				j--;
+			}*/
+		}
+	}
+	return std::accumulate(gays.begin(), gays.end(), 0);
+}
 
 int main() {
+	int count;
+	std::cin >> count;
+	std::vector<int> groups(nGroups);
+	for (int i = 0; i < count; i++) {
+		int val;
+		std::cin >> val;
+		groups[val - 1]++;
+	}
+	std::cout << FindMax(std::move(groups));
 	return 0;
 }
